9b19b8897d3fd92b2d3cfcd80f9e04dd066def85
Pkdotts <43733267+Pkdotts@users.noreply.github.com>
2024-05-07 13:52:16 -0400

    added loading from jsons

--- src/recipe.jsx
+++ src/recipe.jsx
@@ -2,20 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import RecipeTemplate from "./RecipeTemplate.jsx";
-
-var ingredients = ["Lettuce", "Cheese", "Tomato", "Hamburger"];
+import data from "./recipes/testRecipe.json";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <RecipeTemplate
-            title="Fudge"
-            author="Andy"
-            par="our whole universe was in a hot dense state"
-            prepTime="10 mins"
-            cookTime="30 mins"
-            totalTime="40 mins"
-            servings="20"
-            ingredients={ingredients}
-        />
+        <RecipeTemplate data={data} />
     </React.StrictMode>
 );
