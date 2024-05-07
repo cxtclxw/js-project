5f8ea0d2312239648fa5d2a039fce5b8567ad41e
layne <thatbilingual@gmail.com>
2024-05-07 13:31:41 -0400

    footer done

---
+++ src/Footer.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./index.css";
+
+function Footer() {
+  return (
+    <>
+      <footer>
+        <a id="copy">&copy; 2024 Layne Elliott + Andy Bao Le</a>
+        <a id="contact">CONTACT (hover)</a>
+        <a id="hidden">layne@cxtclxw.xyz + andy@exampleweb.xyz</a>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
