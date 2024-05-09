0d92970f6a138be5d7165fad4f333e28314e5310
layne <thatbilingual@gmail.com>
2024-05-09 16:24:27 -0400

    added CSS + readability
    fishe Coloure

--- src/recipe2.jsx
+++ src/recipe2.jsx
@@ -1,16 +1,16 @@
 const JSON = "src/recipes/salad.json";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import RecipeTemplate from "./recipeTemplate.jsx";
+import RecipeTemplate from "./RecipeTemplate.jsx";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import data from "./recipes/salad.json";
-import "./index.css";
+import "./recipe.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <Header />
-        <RecipeTemplate data={data} json={JSON} />
-        <Footer />
-    </React.StrictMode>
+  <React.StrictMode>
+    <Header />
+    <RecipeTemplate data={data} json={JSON} />
+    <Footer />
+  </React.StrictMode>
 );
