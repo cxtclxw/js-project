608cc101d05759114e9e3539f6bef31c03a9d48f
layne <thatbilingual@gmail.com>
2024-05-09 11:18:04 -0400

    Added 'about' lander + better layout

---
+++ src/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./index.css";
+
+function About() {
+  return (
+    <>
+      <body>
+        <p id="about">
+          Hello! We are both chefs that want to explore our skills by emulating
+          our favorite meals from our favorite video games.
+        </p>
+      </body>
+    </>
+  );
+}
+
+export default About;
--- src/recipes.jsx
+++ src/recipes.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
-import About from "./About.jsx";
+import RecipeTiles from "./recipeTiles.jsx";
 import "./index.css";
 
 function App() {
   return (
     <div>
       <Header />
-      <About />
+      <RecipeTiles />
       <Footer />
     </div>
   );
